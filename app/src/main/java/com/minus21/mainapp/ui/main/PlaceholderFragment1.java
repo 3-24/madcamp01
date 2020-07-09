ced769c6dc7e3c682dcb8c6f9158746736f7f88f
Youngseok Choe <yschoelife@gmail.com>
2020-07-09 15:02:23 +0900

    Make listview

--- app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment1.java
+++ app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment1.java
@@ -1,27 +1,41 @@
 package com.minus21.mainapp.ui.main;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.provider.ContactsContract;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.core.content.ContextCompat;
 
 import com.minus21.mainapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment1 extends Fragment {
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter arrayAdapter;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
-    //private PageViewModel pageViewModel;
 
     public static PlaceholderFragment1 newInstance(int index) {
         PlaceholderFragment1 fragment = new PlaceholderFragment1();
@@ -31,29 +45,56 @@ public class PlaceholderFragment1 extends Fragment {
         return fragment;
     }
 
+    String [] arrProjection = {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME
+    };
+
+    private void readContacts() {
+        ContentResolver contentResolver=getActivity().getContentResolver();
+        Cursor cursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,arrProjection,ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1",null,null);
+        if (cursor.moveToFirst()){
+            Log.d("contacts", cursor.getString(0));
+            Log.d("contacts", cursor.getString(1));
+            do {     arrayList.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+            }while (cursor.moveToNext());
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
         int index = 1;
         if (getArguments() != null) {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
-        //pageViewModel.setIndex(index);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        readContacts();
+    }
+
+
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main1, container, false);
-//        final TextView textView = root.findViewById(R.id.section_label);
-//        pageViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
+        listView = root.findViewById(R.id.list);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS}, 100);
+        }
+        else readContacts();
+
         return root;
     }
 }
