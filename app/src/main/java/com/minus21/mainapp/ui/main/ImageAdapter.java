de7548c3dd72197ba541f937001ca543e19321b1
Jeonghyun109 <kjhrlawjdgus@naver.com>
2020-07-11 22:50:42 +0900

    Complete gallery, but slow

---
+++ app/src/main/java/com/minus21/mainapp/ui/main/ImageAdapter.java
@@ -0,0 +1,79 @@
+package com.minus21.mainapp.ui.main;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.minus21.mainapp.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.CustomViewHolder> {
+    private Context context;
+    private ArrayList<String> mList = new ArrayList<String>();
+    public int position;
+
+    public int getPosition() {
+        return position;
+    }
+    public void setPosition() {
+        this.position = position;
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        protected ImageView imageView;
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.imageView = (ImageView) view.findViewById(R.id.i_am_image);
+        }
+    }
+
+    public ImageAdapter(Context context,ArrayList<String> list) {
+        this.context = context;
+        this.mList.clear();
+        this.mList = list;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.gallery_layout, viewGroup, false);
+
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
+        String data = mList.get(position);
+        try {
+
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), Uri.parse(data));
+            ImageView imageView = (ImageView) viewholder.imageView.findViewById(R.id.i_am_image);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != mList ? mList.size() : 0);
+    }
+}
--- app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment2.java
+++ app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment2.java
@@ -1,33 +1,67 @@
 package com.minus21.mainapp.ui.main;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.minus21.mainapp.BuildConfig;
 import com.minus21.mainapp.R;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.lang.annotation.Target;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment2 extends Fragment {
-    private static final int REQUEST_CODE = 0;
-    private ImageView imageView;
-
+    private ArrayList<String> mArrayList = new ArrayList<String>();
+    private ImageAdapter mAdapter;
+    private Context context;
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mLayoutManager;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
     public static PlaceholderFragment2 newInstance(int index) {
@@ -39,44 +73,114 @@ public class PlaceholderFragment2 extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+        //pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+    }
+
+    @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main2, container, false);
-        imageView = root.findViewById(R.id.i_am_image);
 
-        imageView.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent,REQUEST_CODE);
-            }
-        });
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.gallery);
+        mRecyclerView.setHasFixedSize(true);
 
+        int numberOfColumns = 4;
+        mLayoutManager = new GridLayoutManager(context,numberOfColumns);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mArrayList = new ArrayList<>();
+
+        mAdapter = new ImageAdapter(context,mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        checkSelfPermission();
+
+        String[] projection = {
+                MediaStore.Images.Media._ID, MediaStore.Images.Media.DISPLAY_NAME};
+        Cursor cursor = getActivity().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, "UPPER(" + MediaStore.Images.Media.DATE_TAKEN + ") ASC");
+
+        if (cursor.moveToFirst()) {
+            do {
+                Uri ContentUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media._ID)));
+                mArrayList.add(String.valueOf(ContentUri));
+                mAdapter.notifyDataSetChanged();
+//                try {
+//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), ContentUri);
+//                    ImageView imageView = (ImageView) root.findViewById(R.id.i_am_image);
+//                    imageView.setImageBitmap(bitmap);
+//                    mArrayList.add(imageView);
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
 
         return root;
     }
 
-    @Override
-    public void onActivityResult(int requestCode,int resultCode,Intent data) {
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == getActivity().RESULT_OK) {
-                try {
-                    InputStream in = getActivity().getContentResolver().openInputStream(data.getData());
-
-                    Bitmap img = BitmapFactory.decodeStream(in);
-                    in.close();
-
-                    imageView.setImageBitmap(img);
-                } catch (Exception e) {
-
+    // 권한에 대한 응답이 있을 때 작동하는 함수
+//    @Override
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 권한을 허용한 경우
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("MainActivity", "권한 허용 : " + permissions[i]);
                 }
-            }
-            else if (resultCode == getActivity().RESULT_CANCELED) {
-                Toast.makeText(getActivity(),"사진 선택 취소",Toast.LENGTH_LONG).show();
             }
         }
     }
+
+    public void checkSelfPermission() {
+        String temp = "";
+        // 파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+
+        // 파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(getActivity(), temp.trim().split(" "), 1);
+        } else {
+            // 모두 허용 상태
+            Toast.makeText(getActivity(), "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode,int resultCode, @Nullable Intent data) {
+//        if (requestCode == 101 && resultCode == getActivity().RESULT_OK) {
+//            try {
+//                InputStream in = getActivity().getContentResolver().openInputStream(data.getData());
+//
+//                Bitmap bm = BitmapFactory.decodeStream(in);
+//                in.close();
+//                imageView.setImageBitmap(bm);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        else if (requestCode == 101 && resultCode == getActivity().RESULT_CANCELED) {
+//            Toast.makeText(getActivity(),"사진 선택 취소",Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
