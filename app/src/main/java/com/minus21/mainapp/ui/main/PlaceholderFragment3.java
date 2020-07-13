e7a6d90a66abea1bdc9e6239b2dcaecb770d03f6
Youngseok Choe <yschoelife@gmail.com>
2020-07-13 15:42:30 +0900

    Create penweathermap reqeust-response routine with retrofit

--- app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment3.java
+++ app/src/main/java/com/minus21/mainapp/ui/main/PlaceholderFragment3.java
@@ -1,6 +1,7 @@
 package com.minus21.mainapp.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
+import com.google.gson.JsonObject;
 import com.minus21.mainapp.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -31,6 +41,40 @@ public class PlaceholderFragment3 extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main3, container, false);
+        getWeather(0,0);
         return root;
+    }
+
+    private interface ApiService {
+        String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+        String serviceKey = "SECRET";
+        @GET("weather")
+        Call<JsonObject> getWeather ( @Query("q") String city,
+                                      @Query("appid") String appKey);
+    }
+
+    private void getWeather(double latitude, double longitude){
+        Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(ApiService.BASE_URL)
+                .build();
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<JsonObject> call = apiService.getWeather("seoul", ApiService.serviceKey);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(@NonNull Call<JsonObject> call, @NonNull Response<JsonObject> response) {
+                if (response.isSuccessful()) {
+                    JsonObject object = response.body();
+                    Log.d("weather",object.toString());
+                }
+                else{
+                    Log.d("weather", response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t){
+                Log.d("weather",t.toString());
+            }
+        });
     }
 }
